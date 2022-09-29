ff8b205b76b017b28c0e9ec76f68ffe873b41e6a
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-09-29 09:35:04 +0300

    Initial commir with authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def index
+    render json: User.all, status: :ok
+  end
+
+  def show
+    render json: user, status: :ok
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unproceessable_entity
+    end
+  end
+
+  def update
+    unless user.update(user_params)
+      render json: { errors: user.errors.full_messages },status: :unproceessable_entity
+    end
+  end
+
+  def destroy
+    user.destroy
+  end
+
+  private
+
+  def user_params
+    params.permit(:name,:username,:email,:password)
+  end
+
+  def set_user
+    user = User.find(params[:id])
+  end
+end
